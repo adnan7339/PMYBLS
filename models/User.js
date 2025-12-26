12fadc1e9ae445b4b41cb69ff62191932fd5cd3e
adnan7339 <adnan.bakhsh3115@gmail.com>
2025-12-26 20:09:11 +0500

    Add files via upload
    Initial project structure

---
+++ models/User.js
@@ -0,0 +1,8 @@
+
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  role: { type: String, default: "admin" }
+});
+export default mongoose.models.User || mongoose.model("User", UserSchema);
---
+++ pages/search.js
@@ -0,0 +1,1 @@
+export default function Search(){return <h1>Search</h1>}
